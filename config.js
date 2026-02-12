ce159e3cfb75b3ab769e02d9621f5140a97b6087
Jay <92415406+jaybhiesantos@users.noreply.github.com>
2026-02-12 17:52:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.mediafire.com/file/n77iy1qk3i6fqql/Vanilla_Kyanai_%255BMusic_Box%255D.mp3/file", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dq5acn013/video/upload/v1770889928/Vanilla_Kyanai_Music_Box_jke7hk.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
